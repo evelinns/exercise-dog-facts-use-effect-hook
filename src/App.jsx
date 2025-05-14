d3ae4949dfccb6b6da873a3a6e30c1ba8e9ce0f8
evelinns <evelinn.idenfors@gmail.com>
2025-05-14 08:35:42 +0100

    outline for fetching of dog data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 // Import required  hooks
+import { useState, useEffect } from 'react';
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
-  // Hint: Initialize state for storing the dog fact
-  // Hint: Define the API endpoint
-  // Hint: Create a function to fetch the dog fact
-  // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+
+  const [dogFact, setDogFact] = useState();
+  const [updatedogfact, setUpdatedogfact] = useState(0);
+  const dogApi = "https://dogapi.dog/api/v2/facts";
+
+  useEffect(() => {
+    fetch(dogApi)
+      .then(res => res.json())
+      .then(json => setDogFact(json.data[0].attributes.body))
+  }, [updatedogfact])
 
   return (
+
     <div className="App">
-      <DogFact />
+      {dogFact && <DogFact fact={dogFact} />}
+      <button onClick={() => setUpdatedogfact(prev => prev + 1)}>Update Fact</button>
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,9 +1,11 @@
 // DogFact Component
-export const DogFact = () => {
+export const DogFact = (props) => {
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return <div>
+    {props.fact}
+  </div>;
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
